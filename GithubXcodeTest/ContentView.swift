fca1ab43b3e8d355799e144fe21095ebf6995936
Roger Lacson <rogerbayarea@gmail.com>
2020-07-31 17:35:54 -0700

    Initial Commit

---
+++ GithubXcodeTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubXcodeTest
+//
+//  Created by Roger BayArea on 7/31/20.
+//  Copyright © 2020 Roger BayArea. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
